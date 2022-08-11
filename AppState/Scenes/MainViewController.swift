c3dc2eb0086acf3bb18b149f41e996d8d061b68f
Jaewon <woin2ee@gmail.com>
2022-08-11 20:03:49 +0900

    feat : 앱 개발 환경 설정

--- AppState/Scenes/MainViewController.swift
+++ AppState/Scenes/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  AppState
 //
 //  Created by Jaewon on 2022/08/11.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
