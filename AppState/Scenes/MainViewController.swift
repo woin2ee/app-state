397ef8cb7dd0cc1c93e5f2e92ddfbd100acc49f9
Jaewon <woin2ee@gmail.com>
2022-08-13 17:07:32 +0900

    1초마다 찍히는 타이머로 background 상태에 진입하면 더이상 코드가 실행되지 않음을 확인

--- AppState/Scenes/MainViewController.swift
+++ AppState/Scenes/MainViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    private var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
+    }
+    
+    func startTimer() {
+        var count: Int = 0
+        timer = .scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            count += 1
+            print(count)
+        }
     }
 }
