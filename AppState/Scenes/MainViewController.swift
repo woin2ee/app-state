de23c6403b3687629e426ca25bfebcdf08b01cde
Jaewon <woin2ee@gmail.com>
2022-08-17 16:13:54 +0900

    Background Task Completion 실험을 위해 Background 에서 실행되는 서로 다른 2개의 타이머 추가

--- AppState/Scenes/MainViewController.swift
+++ AppState/Scenes/MainViewController.swift
@@ -10,18 +10,50 @@ import OSLog
 
 final class MainViewController: UIViewController {
     
-    private var timer: Timer?
+    private var fastTimer: Timer?
+    private var slowTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startTimer()
+        startFastCounter()
+        startSlowCounter()
     }
     
-    func startTimer() {
+    func startFastCounter() {
         var count: Int = 0
-        timer = .scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+        
+        let identifier = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+            Logger.test.error("주어진 Background Task 실행 시간이 만료되었습니다. (현재 Fast Counter 값: \(count))")
+        })
+        
+        fastTimer = .scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
             count += 1
-            Logger.test.notice("Running count(\(count))")
+            Logger.test.notice("Running Fast Counter(\(count))")
+            
+            if count >= 10 {
+                self.fastTimer?.invalidate()
+                Logger.test.notice("Finish Fast Counter")
+                UIApplication.shared.endBackgroundTask(identifier)
+            }
+        }
+    }
+    
+    func startSlowCounter() {
+        var count: Int = 0
+        
+        let identifier = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+            Logger.test.error("주어진 Background Task 실행 시간이 만료되었습니다. (현재 Slow Counter 값: \(count))")
+        })
+        
+        slowTimer = .scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
+            count += 1
+            Logger.test.notice("Running Slow Counter(\(count))")
+            
+            if count >= 10 {
+                self.slowTimer?.invalidate()
+                Logger.test.notice("Finish Slow Counter")
+                UIApplication.shared.endBackgroundTask(identifier)
+            }
         }
     }
 }
